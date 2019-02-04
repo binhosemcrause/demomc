df7bc63d876bb0738d9bfab604f1c5f2a28e9686
Robson Rafael da Silva <binhosemcrause@gmail.com>
2019-02-04 15:11:46 -0200

    Projeto criado

---
+++ src/main/java/com/morpheus/demomc/DemomcApplication.java
@@ -0,0 +1,14 @@
+package com.morpheus.demomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomcApplication.class, args);
+	}
+
+}
+
