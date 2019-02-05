f7c3683c8c86ad75a0b94e3d225ab2b90d085194
Robson Rafael da Silva <binhosemcrause@gmail.com>
2019-02-05 13:16:19 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/morpheus/demomc/services/CategoriaService.java
+++ src/main/java/com/morpheus/demomc/services/CategoriaService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.morpheus.demomc.domain.Categoria;
 import com.morpheus.demomc.repositories.CategoriaRepository;
+import com.morpheus.demomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -16,6 +18,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
