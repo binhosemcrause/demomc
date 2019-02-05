33fbd2d489f7fdff49173ffa590521a820f2050c
Robson Rafael da Silva <binhosemcrause@gmail.com>
2019-02-05 10:58:30 -0200

    Criando operação de instanciação

--- src/main/java/com/morpheus/demomc/DemomcApplication.java
+++ src/main/java/com/morpheus/demomc/DemomcApplication.java
@@ -1,13 +1,32 @@
 package com.morpheus.demomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemomcApplication {
+import com.morpheus.demomc.domain.Categoria;
+import com.morpheus.demomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class DemomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepostory;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepostory.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
