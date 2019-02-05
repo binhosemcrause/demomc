e874aeb59d63ac96f79799d31b01d244dbae37fd
Robson Rafael da Silva <binhosemcrause@gmail.com>
2019-02-05 11:25:32 -0200

    Produto e associacao muitos para muitos

--- src/main/java/com/morpheus/demomc/DemomcApplication.java
+++ src/main/java/com/morpheus/demomc/DemomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.morpheus.demomc.domain.Categoria;
+import com.morpheus.demomc.domain.Produto;
 import com.morpheus.demomc.repositories.CategoriaRepository;
+import com.morpheus.demomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class DemomcApplication implements CommandLineRunner{
 
 	@Autowired
-	private CategoriaRepository categoriaRepostory;
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemomcApplication.class, args);
@@ -24,8 +28,20 @@ public class DemomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Produto p1 = new Produto(null, "Computador", 200.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		categoriaRepostory.saveAll(Arrays.asList(cat1,cat2));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 		
 	}
 
