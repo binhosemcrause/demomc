83b31d994415a557f20bff8f7ab6d4eb769df342
Robson Rafael da Silva <binhosemcrause@gmail.com>
2019-02-05 15:48:43 -0200

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/morpheus/demomc/DemomcApplication.java
+++ src/main/java/com/morpheus/demomc/DemomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.morpheus.demomc.domain.Categoria;
 import com.morpheus.demomc.domain.Cidade;
+import com.morpheus.demomc.domain.Cliente;
+import com.morpheus.demomc.domain.Endereco;
 import com.morpheus.demomc.domain.Estado;
 import com.morpheus.demomc.domain.Produto;
+import com.morpheus.demomc.domain.enums.TipoCliente;
 import com.morpheus.demomc.repositories.CategoriaRepository;
 import com.morpheus.demomc.repositories.CidadeRepository;
+import com.morpheus.demomc.repositories.ClienteRepository;
+import com.morpheus.demomc.repositories.EnderecoRepository;
 import com.morpheus.demomc.repositories.EstadoRepository;
 import com.morpheus.demomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class DemomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemomcApplication.class, args);
@@ -62,6 +71,17 @@ public class DemomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));	
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "4384753845",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("12323123123", "9684950685"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "234234234", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "34534534", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
